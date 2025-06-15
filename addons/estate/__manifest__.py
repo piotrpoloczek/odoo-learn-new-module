13ab21fe3f778dc74cd50728815fa3ffc877dd8f
Piotr Poloczek <ppoloczek@technomex.pl>
2025-06-15 08:52:52 +0200

    start learn creating new module in odoo

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Real Estate",
+    "summary": "Test module",
+    "version": "17.0.0.0.0",
+    "license": "OEEL-1",
+    "depends": ["crm"],
+    "data": [
+        "security/res_groups.xml"
+    ],
+    "demo": [
+        "demo/demo.xml"
+    ]
+}
---
+++ addons/estate/models/real_estate.py
@@ -0,0 +1,9 @@
+from odoo import fields, models
+
+
+class RealEstate(models.Model):
+    _name = "real.estate"
+    _description = "Test model"
+
+    name = fields.Char(default="House", required=True)
+    price = fields.Float()
